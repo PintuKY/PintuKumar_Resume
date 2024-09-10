7f9c2b150e74fd825ebb6e9e3fb3b4a88f9851a6
kumarpintubkp1997@gmail.com <you@example.com>
2024-09-10 18:50:18 +0530

    update for location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { switchMap } from 'rxjs/operators';
+import { data } from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['../demo-styling.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Pintukumar-resume';
   currentProgress: number = 50;
+  ipAddress: string = ''  
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.loadUserInfo();
+  }
+  loadUserInfo(){
+   
+    this.http
+    .get<{ ip: string }>('https://jsonip.com')
+    .pipe(
+      switchMap((value:any) => {
+        this.ipAddress = value.ip;  
+        let url =`http://api.ipstack.com/${this.ipAddress}?access_key=969b80f4d1f2badf41ff3ebb7feda531`;
+        return this.http.get(url);
+      })
+    )
+    .subscribe(
+      (data) => {
+        //this.ipAddress = data.ip;
+        console.log(data);
+      },
+      (error) => {
+        console.error('Error fetching IP address', error);
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyResumePageComponent } from './my-resume-page/my-resume-page.component';
@@ -14,7 +14,8 @@ import { ProgressBarComponent } from './progress-bar/progress-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
